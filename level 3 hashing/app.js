e5e9c00e9e57a1d71c34df17d0d89fbab00c2bd3
Sarthak Bhatnagar <sarthaksbi@gmail.com>
2020-07-28 00:54:00 +0530

    Auth Project

---
+++ level 3 hashing/app.js
@@ -0,0 +1,110 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+
+const md5 = require("md5");
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true,useUnifiedTopology:true});
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+
+
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("secrets");
+            
+        }
+    });
+});
+
+app.post("/login", function (req,res) {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
+
+    User.findOne({ email: username }, function (err, found) {
+        if (err) {
+            console.log(err);
+        } 
+        else {
+            if (found) {
+                if (found.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port ");
+  });
